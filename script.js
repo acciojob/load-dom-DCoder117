5c5554f59bedc2695b83287344bf53a4f57f801f
DCoder117 <dhirajopjadhav17@gmail.com>
2026-09-09 21:51:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//your JS code here. If required.
-let win = document.querySelector("body");
-
-win.innerText = "DOM load success";
+document.addEventListener("DOMContentLoaded", function () {
+    document.body.innerText = "DOM load success";
+});
